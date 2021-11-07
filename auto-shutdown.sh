78230500941908b7c0e913be397e5bbaa6232a37
Dan Fey <feydanm@gmail.com>
2021-11-07 12:24:39 -0800

    fixing auto shutdown script

--- auto-shutdown.sh
+++ auto-shutdown.sh
@@ -4,21 +4,22 @@ shutdownIdleMinutes=30
 idleCheckFrequencySeconds=1
 
 isIdle=0
-while [  $isIdle -le 0 ]; do
-    idle=1
+while [ $isIdle -le 0 ]; do
+    isIdle=1
     iterations=$(($idleCheckFrequencySeconds * 60 * $shutdownIdleMinutes))
-    while [$counter -lt $iterations]; do
-        connectionBytes=$(ss -lu | grep 777 | awk -F ' ' '{s+=$2} END {print s}')
-        if [$connectionBytes -gt 0]; then
-            idle=0
-            echo "Activity detected, resetting shutdown timer to $shutdownIdleMinutes minutes."
-            break
-        fi
-        counter++
+    while [ $iterations -gt 0 ]; do
         sleep $idleCheckFrequencySeconds
+        connectionBytes=$(ss -lu | grep 777 | awk -F ' ' '{s+=$2} END {print s}')
+        if [ ! -z $connectionBytes ] && [ $connectionBytes -gt 0 ]; then
+            isIdle=0
+        fi
+        if [ $isIdle -le 0 ] && [ $(($iterations % 21)) -eq 0 ]; then
+           echo "Activity detected, resetting shutdown timer to $shutdownIdleMinutes minutes."
+           break
+        fi
+        iterations=$((iterations-1))
     done
 done
 
 echo "No activity detected for $shutdownIdleMinutes, shutting down."
-
-# shutdown now
+sudo shutdown -h now
